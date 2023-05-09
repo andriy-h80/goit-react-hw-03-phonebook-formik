44c9def66ce3aeb7fbf308ff8461a9a228bcbdad
andriy-h80 <andrey.hanzel@gmail.com>
2023-05-09 17:13:59 +0300

    homwork done )))

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,16 +4,28 @@
   import ContactForm from './ContactForm/ContactForm';
   import Filter from './Filter/Filter';
   import ContactList from './ContactList/ContactList';
+  import initialContacts from '../contacts.json';
   
   export class App extends Component {
     state = {
-      contacts: [
-        {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-        {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-        {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-        {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-      ],
+      contacts: [],
       filter: '',
+    }
+
+    componentDidMount() {
+      const savedContacts = localStorage.getItem('contacts')
+      if (savedContacts !== null) {
+        const parsedContacnts = JSON.parse(savedContacts);
+        this.setState( { contacts: parsedContacnts });
+        return;
+      }
+      this.setState({ contacts: initialContacts });
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+      if (prevState.contacts !== this.state.contacts) {
+        localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+      }
     }
 
     handleChange = event => {
--- src/src/components/ContactList/ContactList.styled.jsx
+++
@@ -1,6 +0,0 @@
-import styled from '@emotion/styled';
-
-export const ContactListStyled = styled.ul`
-    margin: 5px;
-    padding: 5px;
-`;
